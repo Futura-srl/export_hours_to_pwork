2b9657284cdaac0de95661d350bc685f5d87e9a6
luca <luca.cocozzza@futurasl.com>
2025-03-04 23:23:15 +0100

    Fix

--- models/trip.py
+++ models/trip.py
@@ -15,10 +15,10 @@ class Trip(models.Model):
         states = self.env['gtms.trip.states'].search([])
         return states.mapped(lambda s: (s.name, s.description))
 
-    
+
     check = fields.Boolean(default=False)
-    trip_start_from_survey = fields.Datetime(states={'checked': [('readonly', False)]})
-    trip_end_from_survey = fields.Datetime(states={'checked': [('readonly', False)]})
+    trip_start_from_survey = fields.Datetime()
+    trip_end_from_survey = fields.Datetime()
     drivers_payment = fields.Selection([('ore_pianificate','Ore pianificate'),('ore_effettive','Ore effettive'),('ore_macarena','Ore Mix 1'),('ore_macarena_inverso','Ore Mix inverso'),('non_pagabile','Non pagare')], default='ore_pianificate')
     all_drivers_ids = fields.One2many('res.partner', compute="_find_all_drivers_ids")
 
@@ -101,7 +101,7 @@ class Trip(models.Model):
             field.all_drivers_ids = list(set(drivers))
 
 
-    
+
     # @api.depends('check','number_of_operations_executed', 'number_of_operation_running',
     #              'number_of_operation_planned', 'number_operation_partially_planned', 'number_of_operations_cancelled',
     #              'is_ready', 'number_of_operations', 'is_canceled')
@@ -138,7 +138,7 @@ class Trip(models.Model):
     #         else:
     #             trip.state = 'draft'
     #             trip.check = False
-    
+
     @api.depends('check','number_of_operations_executed', 'number_of_operation_running',
                  'number_of_operation_planned', 'number_operation_partially_planned', 'number_of_operations_cancelled',
                  'is_ready', 'number_of_operations', 'is_canceled')
@@ -153,12 +153,12 @@ class Trip(models.Model):
                 trip.state = 'checked'
                 trip.is_readonly = True
 
-    
+
     def unchecked(self):
         for record in self:
             # Cerco gli orari inseriti nel Timesheet prima di rimuoverli
             work_times = self.env['account.analytic.line'].search([('gtms_id', '=', record.id)])
-            
+
             # Controllo se ci sono orari già convalidati
             if any(work_time.validated_status in ['validated','processed','done'] for work_time in work_times):
                 raise ValidationError(_("Il viaggio contiene degli orari già convalidati su Pwork"))
@@ -166,8 +166,8 @@ class Trip(models.Model):
             # Rimuovo gli orari dal timesheet
             work_times.unlink()
             record.check = False
-    
-    
+
+
     def checked(self):
         for record in self:
             if record.state == 'checked':
@@ -177,7 +177,7 @@ class Trip(models.Model):
             trip_type_id = record.trip_type_id.id
             task_id = self.env['gtms.trip.type'].search_read([('id', '=', trip_type_id)], ['task_id'])[0]['task_id'][0]
             project_id = self.env['project.task'].search_read([('id', '=', task_id)], ['project_id'])[0]['project_id'][0]
-            
+
             _logger.info(record)
             _logger.info(record.state)
             _logger.info(trip_type_id)
@@ -189,8 +189,8 @@ class Trip(models.Model):
             #     raise ValidationError(_(f"Il viaggio {trip} con id {id} deve prima essere eseguito"))
             driver_id = 0
             laerning_driver_id = 0
-            
-            
+
+
             company_id = record.company_ids[0].id
             driver_payment = record.drivers_payment
             if driver_payment == False or driver_payment == False:
@@ -198,7 +198,7 @@ class Trip(models.Model):
             _logger.info(driver_id)
             _logger.info(company_id)
             _logger.info(trip)
-            
+
             # Raccolgo il datetime in base al metodo scelto
             if driver_payment == "ore_effettive":
                 if record.trip_start_from_survey == False or record.trip_end_from_survey == False:
@@ -246,7 +246,7 @@ class Trip(models.Model):
 
             work_time = end_datetime - start_datetime
             working_seconds = work_time.total_seconds() / 3600.0
-            
+
             # Cerco gli autisti che hanno guidato durante il viaggio
             drivers = self.env['gtms.trip.vehicle.manager'].search_read([('trip_id', '=', id)],['driver_id', 'learning_driver_id'])
             # drivers = list({tuple(driver.items()) for driver in drivers})
@@ -257,8 +257,8 @@ class Trip(models.Model):
                 _logger.info(driver)
                 _logger.info(driver['driver_id'])
                 _logger.info(driver['learning_driver_id'])
-            
-            
+
+
             # Cerco il dipendente con contratto attivo al momento della partenza del viaggio
             employees = self.env['hr.employee'].search([('address_home_id', '=', driver_id), '|', ('active', '=', False),('active', '=', True)])
             if driver['learning_driver_id']:
@@ -281,11 +281,11 @@ class Trip(models.Model):
                 ])
                 _logger.info("XXXXXXXXXXXXX")
                 _logger.info(contracts)
-                
+
                 if contracts:
                     _logger.info(contracts[0].employee_id.id)
                     employee_id = contracts[0].employee_id.id
-                    
+
 
                     # Creo il Timesheet
                     timesheet = self.env['account.analytic.line'].create(
@@ -300,14 +300,14 @@ class Trip(models.Model):
                             'name': trip,
                             'gtms_id': id,
                         })
-                    # self.is_readonly = True 
+                    # self.is_readonly = True
                     self.check = True
-                    
+
                 else:
                     continue
                 if indice == len(employees) and not contracts and not timesheet:
                     raise ValidationError(_(f"Il dipendente {employee.name} con id {employee.id} attualmente non ha alcun contratto valido. Contattare l'assistenza fornendo i dati appena forniti."))
-                    
+
             if driver['learning_driver_id']:
                 for employee in employees_learning:
                     contracts = self.env['hr.contract'].search([
@@ -343,5 +343,3 @@ class Trip(models.Model):
         #     _logger.info(record.id)
         #     drivers = self.env['gtms.trip'].search_read([('id', '=', record.id)],['name','activity_calendar_event_id','drivers_payment', 'delivery_note_ids','drivers_ids'])
         #     _logger.info(drivers)
-
-
