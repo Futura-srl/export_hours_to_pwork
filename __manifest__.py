985260df7ecb206e6c7b5ee76fd6fd32f2b34b67
Luca Cocozza <luca.cocozza@futurasl.com>
2025-07-30 16:24:17 +0200

    Fix inserito debugger per capire errore

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'application': True,
     'description': "Con questo modulo è possibile esportare le ore dal Tms a Pwork",
     'depends': [
-        'gtms','hr_timesheet', 'gtms_inspection_survey', 'gtms_fleet_organization','carburante', 'base_automation', 'Diritti'],
+        'base','gtms','hr_timesheet', 'gtms_inspection_survey', 'gtms_fleet_organization','carburante', 'base_automation', 'Diritti'],
     'data': [
         # # # Settaggi per accesso ai contenuti
         'data/ir.model.access.csv',
