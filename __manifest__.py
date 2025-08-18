24d4257077008b97324de560411b4ff7fe6a5192
Luca Cocozza <luca.cocozza@futurasl.com>
2025-08-18 10:11:19 +0200

    Added dependency

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'application': True,
     'description': "Con questo modulo è possibile esportare le ore dal Tms a Pwork",
     'depends': [
-        'base','gtms','hr_timesheet', 'gtms_inspection_survey', 'gtms_fleet_organization','carburante', 'base_automation', 'Diritti'],
+        'base','gtms','hr_timesheet', 'gtms_inspection_survey', 'gtms_fleet_organization','carburante', 'base_automation', 'Diritti', 'fleet'],
     'data': [
         # # # Settaggi per accesso ai contenuti
         'data/ir.model.access.csv',
