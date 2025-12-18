a2bc75a7e575990a56d06626cf5d8ac0563909bf
Luca Cocozza <luca.cocozza@futurasl.com>
2025-12-18 14:13:30 +0100

    Fix recupero badge tramite contratto attivo

--- models/account_analytic_line_pwork.py
+++ models/account_analytic_line_pwork.py
@@ -80,7 +80,7 @@ class AccountAnalyticLine(models.Model):
             # Cerco il contratto attivo del dipendente nella data
                 contract = self.env['hr.contract'].search([('employee_id', '=', record.employee_id.id), ('date_start', '<=', record.datetime_start.date()), '|', ('date_end', '>=', record.datetime_start.date()), ('date_end', '=', False)], limit=1)
                 if contract:
-                    badges = self.env['hr.badgespwork'].search([('contract_ids', '=', contract),('active', '=', True), ('valid_from', '<=', record.datetime_start.date()), '|', ('valid_to', '>=', record.datetime_start.date()), ('valid_to', '=', False)], limit=1)
+                    badges = self.env['hr.badgespwork'].search([('contract_ids', '=', contract.id),('active', '=', True), ('valid_from', '<=', record.datetime_start.date()), '|', ('valid_to', '>=', record.datetime_start.date()), ('valid_to', '=', False)], limit=1)
             ######
             ######
             for badge in badges:
