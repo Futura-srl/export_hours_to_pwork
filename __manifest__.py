6ad6538031a3b4e79a3ee9b6d07a11fe6fc2139c
Luca Cocozza <luca.cocozza@futurasl.com>
2026-06-10 10:13:58 +0200

    Aggiunti 2 campi opzionali alla tree view di gtms.trip

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Export hours to Pwork',
-    'version': '17.2',
+    'version': '17.0.2.1.0',
     'author': "Luca Cocozza",
     'application': True,
     'description': "Con questo modulo è possibile esportare le ore dal Tms a Pwork",
